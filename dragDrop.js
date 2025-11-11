ef5c1748aeb610e80caaa0971c2b27aac640ab59
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2025-11-11 13:32:06 -0300

    fix: corrige lógica do drag and drop

--- dragDrop.js
+++ dragDrop.js
@@ -1,25 +1,31 @@
 const draggable = document.querySelectorAll(".draggable");
 const dropzone = document.querySelectorAll(".dropzone");
 
-draggable.addEventListener('dragstart', (event) =>{
-    event.dataTransfer.setData('text/plain', event.target.id);
-    draggable.classlist.add('dragging');
+draggable.forEach( (draggable) => {
+    draggable.addEventListener('dragstart', (event) =>{
+    event.dataTransfer.setData('text', event.target.id);
+    draggable.classList.add('dragging');
+    });
 });
 
-draggable.addEventListener('dragend', () =>{
-    draggable.classlist.remove('dragging');
+dropzone.forEach( (dropzone) =>{
+
+    dropzone.addEventListener('dragover', (event) =>{
+        event.preventDefault();
+    });
+
+
+    dropzone.addEventListener('drop', (event) =>{
+        event.preventDefault();
+        const data = event.dataTransfer.getData('text');
+        const dataDrop = document.getElementById(data);
+
+        dataDrop.classList.add('inside');
+        dropzone.classList.add('inside');
+
+        dropzone.appendChild(dataDrop);
+    });
+
 });
 
-dropzone.addEventListener('dragenter', (event) =>{
-    event.preventDefault();
-    dropzone.classlist.add('over');
-});
 
-dropzone.addEventListener('dragover', (event) =>{
-    event.preventDefault();
-});
-
-dropzone.addEventListener('dragleave', () =>{
-    dropzone.classlist.remove('over');
-    dropzone.appendChild(draggable);
-})
