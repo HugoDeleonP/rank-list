c6f2863ba3c08cbc7c859c0a31ee818c4b6427b3
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2025-11-13 19:27:54 -0300

    fix: resolve área de colocar imagem

--- dragDrop.js
+++ dragDrop.js
@@ -1,10 +1,12 @@
 const draggable = document.querySelectorAll(".draggable");
 const dropzone = document.querySelectorAll(".dropzone");
+const images = document.getElementsByClassName("image");
+const galeria = document.getElementById('default-image');
 
 draggable.forEach( (draggable) => {
     draggable.addEventListener('dragstart', (event) =>{
-    event.dataTransfer.setData('text', event.target.id);
-    draggable.classList.add('dragging');
+        event.dataTransfer.setData('text', event.target.id);
+        draggable.classList.add('draggable');
     });
 });
 
@@ -14,18 +16,20 @@ dropzone.forEach( (dropzone) =>{
         event.preventDefault();
     });
 
-
     dropzone.addEventListener('drop', (event) =>{
         event.preventDefault();
         const data = event.dataTransfer.getData('text');
         const dataDrop = document.getElementById(data);
+        const dataParent = dataDrop.parentElement;
 
         dataDrop.classList.add('inside');
         dropzone.classList.add('inside');
 
         dropzone.appendChild(dataDrop);
+
+        if(dataParent.childElementCount == 0){
+            dataParent.classList.remove('inside');
+        }
     });
 
 });
-
-
